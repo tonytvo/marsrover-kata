796329731767d1d440c5acbda9e4ad754635fced
Tony Vo <ttrung.vo@gmail.com>
2022-11-12 08:58:09 -0700

    r inline variable

--- typescript/test/MarsRover.test.ts
+++ typescript/test/MarsRover.test.ts
@@ -1,16 +1,14 @@
-import { describe, test, expect } from "@jest/globals";
+import { describe, expect, test } from "@jest/globals";
 import { MarsRover, Position } from "../src/MarsRover";
 
 
 describe("MarsRover", () => {
   test("given the rover is created, it should have starting point of 0, 0, N", () => {
-    let marsRover = new MarsRover();
-    expect(JSON.stringify(marsRover)).toEqual(JSON.stringify(MarsRover.of(new Position(0, 0, "N"))));
+    expect(JSON.stringify(new MarsRover())).toEqual(JSON.stringify(MarsRover.of(new Position(0, 0, "N"))));
   });
 
   test.skip("given the rover rotate right from north, it should point to east", () => {
-    let marsRover = new MarsRover();
-    expect(JSON.stringify(marsRover.rotate("R"))).toEqual(JSON.stringify(new Position(0, 0, "E")));
+    expect(JSON.stringify(new MarsRover().rotate("R"))).toEqual(JSON.stringify(new Position(0, 0, "E")));
   });
 
 });
