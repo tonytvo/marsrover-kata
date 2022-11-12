c1fe0d4c55256e4c574a62706234918620ca8d8b
Tony Vo <ttrung.vo@gmail.com>
2022-11-11 21:43:35 -0700

    add typescript

---
+++ typescript/test/MarsRover.test.ts
@@ -0,0 +1,8 @@
+import { describe, test } from "@jest/globals";
+import { verify } from "approvals/lib/Providers/Jest/JestApprovals";
+import { Inconsistent } from "../src/Inconsistent";
+
+
+describe("MarsRover", () => {
+  test("move", () => {});
+});
