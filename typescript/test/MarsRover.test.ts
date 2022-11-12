aaa0005df193222df99c96d16d54eee57326bece
Tony Vo <ttrung.vo@gmail.com>
2022-11-12 12:05:40 -0700

    r move factory method to factory class

--- typescript/test/MarsRover.test.ts
+++ typescript/test/MarsRover.test.ts
@@ -5,25 +5,25 @@ class MarsRoverFactory {
   static createMarsRoverFrom(position: Position) {
     return MarsRover.of(position);
   }
+
+  static createDefaultMarsRover() {
+    return new MarsRover();
+  }
 }
 
 describe("MarsRover", () => {
 
   test("given the rover is created, it should have starting point of 0, 0, N", () => {
     let position = new Position(0, 0, "N");
-    expect(JSON.stringify(createDefaultMarsRover())).toEqual(JSON.stringify(MarsRoverFactory.createMarsRoverFrom(position)));
+    expect(JSON.stringify(MarsRoverFactory.createDefaultMarsRover())).toEqual(JSON.stringify(MarsRoverFactory.createMarsRoverFrom(position)));
   });
 
-  function createDefaultMarsRover() {
-    return new MarsRover();
-  }
-
   test("given the rover rotate right from north, it should point to east", () => {
-    expect(JSON.stringify(createDefaultMarsRover().rotate("R"))).toEqual(JSON.stringify(MarsRoverFactory.createMarsRoverFrom(new Position(0, 0, "E"))));
+    expect(JSON.stringify(MarsRoverFactory.createDefaultMarsRover().rotate("R"))).toEqual(JSON.stringify(MarsRoverFactory.createMarsRoverFrom(new Position(0, 0, "E"))));
   });
 
   test("given the rover rotate left from north, it should point to west", () => {
-    expect(JSON.stringify(createDefaultMarsRover().rotate("L"))).toEqual(JSON.stringify(MarsRoverFactory.createMarsRoverFrom(new Position(0, 0, "W"))));
+    expect(JSON.stringify(MarsRoverFactory.createDefaultMarsRover().rotate("L"))).toEqual(JSON.stringify(MarsRoverFactory.createMarsRoverFrom(new Position(0, 0, "W"))));
   });
 
 });
